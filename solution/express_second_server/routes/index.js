1a184cf286f9c6f2fa9e4a614c2eccee2c6d3c15
Marvel <marvelous.asueninmhen@edu.unito.it>
2024-07-05 17:56:59 +0200

    - adjusted routes connnections between servers, by simply moving files. - i had previously made it in a way that allowed the second server to make calls to spring servere, which was against the assignment

--- solution/express_second_server/routes/index.js
+++ solution/express_second_server/routes/index.js
@@ -13,11 +13,6 @@ const APPEARANCE_ROUTER = require('./appearances');
 const CLUB_GAMES_ROUTER = require('./club_games');
 const GAME_ROUTER = require('./game');
 const GAME_EVENT_ROUTER = require('./game_event');
-const CLUB_ROUTER = require('./club');
-const COMPETITIONS_ROUTER = require('./competition');
-const GAME_LINEUP_ROUTER = require('./games_lineup');
-const PLAYER_ROUTER = require('./player');
-const PLAYER_VALUATION_ROUTER = require('./player_valuation');
 const ROOM_LIST_ROUTER = require('./room');
 
 
@@ -27,17 +22,6 @@ router.use('/api', CLUB_GAMES_ROUTER);
 router.use('/api', GAME_ROUTER);
 router.use('/api', GAME_EVENT_ROUTER);
 
-
-/**
- *
- * the following are routes that make calls to a spring server
- */
-router.use('/api', CLUB_ROUTER);
-router.use('/api', COMPETITIONS_ROUTER);
-router.use('/api', GAME_LINEUP_ROUTER);
-router.use('/api', PLAYER_ROUTER);
-router.use('/api', PLAYER_VALUATION_ROUTER);
-router.use('/api', ROOM_LIST_ROUTER);
 
 /* GET home page. */
 /**
